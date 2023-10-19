4369dabcb54980fefc274f539c73404b15f411c6
Kayque Santos <119762234+KayqueSanto@users.noreply.github.com>
2023-10-19 11:19:34 -0300

    Inclusão de Video

--- script1.js
+++ script1.js
@@ -4,16 +4,24 @@ $(document).ready(function() {
     });
 
     function exibirPergunta() {
+        $('#popupButton').hide(); // Oculta o botão "Clique aqui"
         $('#myModal').modal('show');
 
         $('#simButton').click(function() {
             alert("Acho bom mesmo viu, rum.");
             $('#myModal').modal('hide');
+            exibirVideo();
         });
 
         $('#naoButton').click(function() {
             $('#pergunta').text("Tu me ama?");
             $('#emoji').removeClass().addClass('far fa-smile'); // Emoji de risada
         });
+    }
+
+    function exibirVideo() {
+        $('#videoContainer').show();
+        // reproduzir o vídeo automaticamente, descomente a linha abaixo:
+        // document.getElementById('myVideo').play();
     }
 });
